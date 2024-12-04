12a7c76a432078fae44dcc2ed6dbd4cc2228725e
neon443 <69979447+neon443@users.noreply.github.com>
2024-12-04 20:03:44 +0000

    Initial Commit

---
+++ Base4Meow/Base4MeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Base4MeowApp.swift
+//  Base4Meow
+//
+//  Created by Nihaal Sharma on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Base4MeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Base4MeowTests/Base4MeowTests.swift
@@ -0,0 +1,16 @@
+//
+//  Base4MeowTests.swift
+//  Base4MeowTests
+//
+//  Created by Nihaal Sharma on 04/12/2024.
+//
+
+import Testing
+
+struct Base4MeowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
